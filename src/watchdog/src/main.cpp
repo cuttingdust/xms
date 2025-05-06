9f6c02d88324e5edd45e8d24369f580e59fedeeb
cuttingDust_47 <18862963156@163.com>
2025-05-06 09:58:56 +0800

    兼容mac and linux

--- src/watchdog/src/main.cpp
+++ src/watchdog/src/main.cpp
@@ -1,8 +1,83 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cstring>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
-int main(int argc, char *argv[])
+auto join(const std::vector<std::string>& strings, const std::string& delimiter) -> std::string
 {
-    std::cout << "hello world" << std::endl;
+    std::ostringstream oss;
+
+    for (size_t i = 0; i < strings.size(); ++i)
+    {
+        oss << strings[i];
+        if (i < strings.size() - 1)
+        { /// 在元素之间添加分隔符
+            oss << delimiter;
+        }
+    }
+
+    return oss.str(); /// 返回连接后的字符串
+}
+
+
+void startProcess(const std::vector<std::string>& command)
+{
+    pid_t pid = fork();
+    if (pid == 0)
+    { // 子进程
+        // 使用 execvp 执行命令
+        std::vector<char*> args;
+        for (const auto& arg : command)
+        {
+            std::cout << arg;
+            args.push_back(const_cast<char*>(arg.c_str()));
+        }
+        args.push_back(nullptr); // execvp 需要以 nullptr 结束参数列表
+        execvp(args[0], args.data());
+        // 如果 execvp 返回，说明出错
+        std::cerr << "Failed to execute: " << command[0] << std::endl;
+        exit(1);
+    }
+    else if (pid < 0)
+    {
+        std::cerr << "Fork failed" << std::endl;
+    }
+    // 父进程继续
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 3)
+    {
+        std::cout << "Usage: ./watchdog <interval> <command> [args...]" << std::endl;
+        return -1;
+    }
+
+    int                      interval = atoi(argv[1]);
+    std::vector<std::string> command(argv + 2, argv + argc); // 读取命令及参数
+
+    std::cout << "Monitoring command: " << command[0] << std::endl;
+
+    // 创建新的会话
+    if (fork() > 0)
+        exit(0); // 父进程退出
+    setsid();
+    umask(0);
+
+    std::cout << join(command, " ") << std::endl;
+    //    while (true)
+    //    {
+    //        startProcess(command);
+    //
+    //        // 等待子进程结束
+    //        int status;
+    //        wait(&status);   // 阻塞直到子进程退出
+    //        sleep(interval); // 等待指定的时间
+    //    }
 
     return 0;
 }
